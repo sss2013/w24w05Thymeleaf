48b35f2b58d311365dc64e8a5f36470c98bf39e6
sss2013 <kshi9478@gmail.com>
2024-10-02 16:32:07 +0900

    thymeleaf first

--- src/main/kotlin/kr/ac/kumoh/s20190645/w24w05hymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20190645/w24w05hymeleaf/controller/SongController.kt
@@ -1,12 +1,14 @@
 package kr.ac.kumoh.s20190645.w24w05hymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SongController {
     @GetMapping("/song/random")
-    fun getRandomSong(): String {
+    fun getRandomSong(model : Model): String {
+        model.addAttribute("songTitle","Diver")
         return "random"
     }
 }
